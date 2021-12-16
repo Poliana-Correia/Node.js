6e9454fa342bb0fa9d50999fc9a851230659633f
Poliana Correia <poliana.karenn05@gmail.com>
2021-12-16 18:18:27 -0300

    praticando node

---
+++ require.js
@@ -0,0 +1,9 @@
+// Modulos nativos
+// cont path = required('path')
+
+//console.log(path.basename(_filename))
+
+// Meus modulos
+const myModule = require('./Praticando/exports')
+
+console.log(myModule)
